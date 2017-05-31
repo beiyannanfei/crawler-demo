eb8f7d3b871bc608b1b0e35532d0d4d2fba875ee
wangyanqiang <wangyanqiang@sensoro.com>
2017-05-31 14:47:49 +0800

    init

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 const superagent = require("superagent");
 const cheerio = require("cheerio");
 const url = require("url");
+const eventproxy = require("eventproxy");
 
 const targetUrl = "https://cnodejs.org/";
 
@@ -24,9 +25,47 @@ function parseHtml(text) {
 	$("#topic_list .topic_title").each((idx, element) => {
 		// console.log(element);
 		let $element = $(element);
+		// console.log($element);
 		// console.log($element.attr("href"));
 		let href = url.resolve(targetUrl, $element.attr("href"));
-		console.log(href);
+		// console.log(href);
 		topicUrls.push(href);
+	});
+	// console.log(topicUrls);
+	return getTopicContent(topicUrls);
+}
+
+function getTopicContent(topicUrls) {
+	let ep = new eventproxy();
+
+	ep.after("topic_html", topicUrls.length, topics => {
+		topics = topics.map(topicPair => {
+			let topicUrl = topicPair[0];
+			let topicHtml = topicPair[1];
+			let $ = cheerio.load(topicHtml);
+			let obj = {
+				title: $('.topic_full_title').text().trim(),
+				href: topicUrl,
+				comment1: $('.reply_content').eq(0).text().trim(),
+				comment2: $('.reply_content').eq(1).text().trim()
+			};
+			if ($('.reply_author').get(0) && $('.reply_author').get(0).attribs && $('.reply_author').get(0).attribs.href) {
+				let userHref = url.resolve(targetUrl, $('.reply_author').get(0).attribs.href);
+				obj.userHref = userHref;
+				let userName = $('.reply_author').get(0).children[0].data;
+				obj.userName = userName;
+			}
+			return (obj);
+		});
+		console.log("outcome:");
+		console.log(topics);
+	});
+
+	topicUrls.forEach(topicUrl => {
+		superagent.get(topicUrl)
+			.end((err, res) => {
+				console.log("fetch %j successful", topicUrl);
+				ep.emit("topic_html", [topicUrl, res.text]);
+			});
 	});
 }
